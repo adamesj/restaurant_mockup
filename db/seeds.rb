260b0485616e1c8be90cc96612717a57565db639
Jimmy Adames <jimmy.adames@gmail.com>
2017-07-19 12:14:55 -0400

    Added additional resources for the lesson

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Restaurant.create(
+    name:              Faker::Simpsons.location,
+    street_address:    Faker::Address.street_address,
+    zip_code:          Faker::Address.zip_code
+  )
+end
