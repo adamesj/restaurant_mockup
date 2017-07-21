76a32dfdb056e929bd00e6ff5ab1d9c872fb4cf0
Jimmy Adames <jimmy.adames@gmail.com>
2017-07-21 15:52:40 -0400

    Refactored reservations folder and added captcha gem

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,41 +1,67 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
-  def index
-    @restaurants = Restaurant.all
+  def favorite
+    @restaurant = Restaurant.find(params[:id])
+    type = params[:type]
+    if type == "favorite"
+      current_user.favorites << @restaurant
+      redirect_to :back, notice: "You favorited #{@restaurant.name}"
+
+    elsif type == "unfavorite"
+      current_user.favorites.delete(@restaurant)
+      redirect_to :back, notice: "Unfavorited #{@restaurant.name}"
+
+    else
+      # Type missing, nothing happens
+      redirect_to :back, notice: 'Nothing happened.'
+    end
   end
 
-  def show; end
+  def index
+    @restaurants = Restaurant.all
+    @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
+      restaurant_path = view_context.link_to restaurant.name, restaurant_path(restaurant)
+      marker.lat restaurant.latitude
+      marker.lng restaurant.longitude
+      marker.infowindow "<b>#{restaurant_path}</b>"
+    end
+  end
 
   def new
     @restaurant = Restaurant.new
   end
 
   def create
-    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant = current_user.restaurants.build(restaurant_params)
     if @restaurant.save
-      flash[:notice] = "Successfully added a new restaurant."
-      redirect_to root_path
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
     else
-      flash[:alert] = "Oops something went wrong. Couldn't add this restaurant."
       render :new
     end
   end
 
-  def edit; end
+  def show
+    @restaurant = Restaurant.includes(:categories).find(params[:id])
+    @restaurant.favorited_by
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
 
   def update
-    if @restaurant.update_attributes(restaurant_params)
-      flash[:notice] = "Successfully added a new restaurant."
-      redirect_to restaurants_path
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
-    redirect_to restaurants_path
-    flash[:notice] = "Resturant has been deleted."
+    redirect_to restaurants_url
   end
 
   private
