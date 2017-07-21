d061065bbfb215130e3cd7ef6ec790873b55dd42
Mar Gabriel Tabilla <gabytabilla@gmail.com>
2017-07-21 16:09:11 -0400

    added retaurant foreign id to categ

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721194722) do
+ActiveRecord::Schema.define(version: 20170721200826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_categories_on_restaurant_id"
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.integer "party"
@@ -51,6 +59,7 @@ ActiveRecord::Schema.define(version: 20170721194722) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "restaurants"
   add_foreign_key "reservations", "restaurants"
   add_foreign_key "restaurants", "users"
 end
