1b05ba8c1963416343cf56d2495ae757ac259911
Jimmy Adames <jimmy.adames@gmail.com>
2017-07-21 15:36:32 -0400

    Fixed merge conflicts in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
-=======
   resources :reservations
->>>>>>> 98656bfb9c6c28e9b633911451f05b330a5ba696
   root 'restaurants#index'
   resources :restaurants
 end
